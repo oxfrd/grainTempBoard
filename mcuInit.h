bc82837d3a837571465b10732465bbc0aba8cea1
oxfrd <olaf.chojnacki04@wp.pl>
2025-02-18 18:57:05 +0100

    Initial commit

---
+++ mcuInit.h
@@ -0,0 +1,37 @@
+//created by Oxfrd 8.11.2023
+#pragma once
+
+#include "IMcu.h"
+
+#define HSE_VALUE 32768000
+
+namespace mcu
+{
+    enum class eMcuResources 
+    {
+        ePortB,
+        ePortE,
+        eGPIO_E8,
+        eGPIO_B2,
+        eTimer2,
+        eIntTim2,
+        ePortA,
+        eGPIO_A0,
+        eGPIO_A1,
+        eGPIO_A2,
+        eGPIO_A3,
+        eGPIO_A5,
+        ePortD,
+        eGPIO_D5,
+        eGPIO_D6,
+        eUART2,
+        eIntUART2,
+        eGPIO_B6,
+        eGPIO_B7,
+        eI2c1,
+        eBMP280,
+        eInterruptI2C1Event,
+    };
+
+    std::shared_ptr<hal::mcu::mcuManager> init();
+}   //mcu
